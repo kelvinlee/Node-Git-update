69926de9e56c0ff35a384df29f043c4d99d909fb
Kelvin <lhq945@gmail.com>
2017-11-10 21:44:14 +0800

    remove self server

--- app.js
+++ app.js
@@ -12,8 +12,7 @@ function gitpull(pathname) {
   console.log("cd "+pathname+" && git pull");
   exec("cd "+pathname+" && git pull",finished);
   // exec("cd /my/demo/douban && git pull");
-
-  send.post("http://47.96.29.129:9998"+pathname)
+  // send.post("http://47.96.29.129:9998"+pathname)
   send.post("http://120.26.226.79:9998"+pathname)
   return true;
 }
